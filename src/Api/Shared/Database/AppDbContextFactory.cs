8c672d13b600f18330d3ea6291e176e02c8da045
Nguyen, Tin Anh <anhtin95@gmail.com>
2025-03-15 17:50:33 +0100

    Fix connection string for AppDbContextFactory

--- src/Api/Shared/Database/AppDbContextFactory.cs
+++ src/Api/Shared/Database/AppDbContextFactory.cs
@@ -6,13 +6,22 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
 {
     public AppDbContext CreateDbContext(string[] args)
     {
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json")
-            .Build();
-
-        var connectionString = configuration.GetConnectionString("Default")!;
+        var connectionString = ResolveConnectionString();
         var dbContextOptions = AppDbContext.ConfigurePostgreSql(connectionString).Options;
         return new AppDbContext(dbContextOptions);
+    }
+
+    private static string ResolveConnectionString()
+    {
+        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        var configurationBuilder = new ConfigurationBuilder();
+        configurationBuilder.SetBasePath(Directory.GetCurrentDirectory());
+        configurationBuilder.AddJsonFile("appsettings.json");
+        if (environment == "Development")
+            configurationBuilder.AddJsonFile("appsettings.Development.json");
+
+        var configuration = configurationBuilder.Build();
+        return configuration.GetConnectionString("Default")!;
     }
 }
